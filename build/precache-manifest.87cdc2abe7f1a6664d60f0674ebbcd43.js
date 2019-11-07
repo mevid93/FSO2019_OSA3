28feb10c07cef44cf683b6f251010d3e2c384e50
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-11-07 15:03:24 +0200

    Osa3: 3.11

---
+++ build/precache-manifest.87cdc2abe7f1a6664d60f0674ebbcd43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42304fc574824f8dd559a5b1f486f540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "296b470e41ab6610c8ee",
+    "url": "/static/css/main.b5613c4b.chunk.css"
+  },
+  {
+    "revision": "332e94772b7fef02148c",
+    "url": "/static/js/2.923ab7fc.chunk.js"
+  },
+  {
+    "revision": "296b470e41ab6610c8ee",
+    "url": "/static/js/main.6adc6467.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
