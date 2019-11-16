0f8802229341244776b9373bfb2799d3a5b85587
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-11-16 15:04:26 +0200

    Osa3: 3.17

--- index.js
+++ index.js
@@ -79,6 +79,19 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(result => {
